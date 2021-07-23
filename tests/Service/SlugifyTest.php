e98e1c0727d19ecd7de940ed76cbdb1b77a1cee5
Martin <martin.lang67@gmail.com>
2021-07-23 10:03:10 +0200

    test

---
+++ tests/Service/SlugifyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Tests\Service;
+
+
+use App\Service\Slugify;
+
+class SlugifyTest extends \PHPUnit\Framework\TestCase
+{
+    public function TestSlugify()
+    {
+        $slugify = new Slugify();
+        $this->assertEquals('et-amet-commodi-nam-a-modi', $slugify->generate('Et amet commodi nam a modi.'));
+    }
+}
