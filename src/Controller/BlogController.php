7a842c14e36f6af4ef59ab008654704ed063fed3
Martin <martin.lang67@gmail.com>
2021-07-26 10:50:45 +0200

    Comment Code

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -50,15 +50,32 @@ class BlogController extends AbstractController
      * @Route("/blog/new", name="blog_new")
      * @Route("/blog/{slug}/edit", name="blog_edit")
      */
+
+    //injection de dépendance
     public function form(Article $article = null, Request $request, EntityManagerInterface $manager) {
+
+        // J'instencie un nouvel objet de la class(ou entité)  USER
 
         if(!$article) {
             $article = new Article();
         }
 
+        //J'utilise la fonction createForm pour instancier le formulaire en lui passant en parametre le nom de la
+        // classe du formulaire ainsi que la variable
+        // dans laquelle est mon objet, afin d'avoir acces à ses fonctions.
+
         $form = $this->createForm(ArticleType::class, $article);
 
+        // handleRequest permet de gérer le traitement de la saisie du formulaire par l'utilisateur.
+        //  On lui passe en parametre l'injection de dépendance $request.
+
         $form->handleRequest($request);
+
+        //C’est l’instruction $form->isSubmitted() qui permet de savoir si le formulaire a été
+        // saisi et si de plus les règles de validations sont vérifiées
+        // ($form->isValid()) alors l’enregistrement sera ajouté à la base de données avant de rediriger
+        // la requête vers un affichage de la liste des locations.
+
 
         if($form->isSubmitted() && $form->isValid()) {
             $slug = $this->slugify->generate($article->getTitle());
@@ -66,12 +83,23 @@ class BlogController extends AbstractController
 
             $article->setCreatedAt((new \DateTime()));
 
+            // Le persist va dire que l'entité qu'on lui passe en parametre, dans ce cas $user est lié à la bdd
+
             $manager->persist($article);
+
+            //Envoi les infos saisies dans la base de donnée. A partir de la, l'objet est converti en requete SQL
+
             $manager->flush();
+
+            //le redirecttoroute fait une redirection web vers une route ici 'security_login'
 
             return $this->redirectToRoute('blog_show', ['slug' => $article->getSlug()]);
         }
 
+        //render fait une redirection vers un moteur de template twig
+        //'form est le nom qu'on donne au formulaire afin de pouvoir l'appeler dans twig
+        //'form' est donc stocké dans une variable $form à qui je dis de créer la vue. Ensuite il faudra aller dans twig créer le formulaire
+        //en faisant {{form(form)}}, on met form entre parentheses car il est égal à 'form'
 
         return $this->render('blog/new.html.twig', [
             'formArticle' => $form->createView(),
